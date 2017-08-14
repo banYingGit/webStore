783a6fc18d3ea7659269da28e21eb7d2000e245d
banYingGIt <635221846@qq.com>
2017-08-14 15:22:08 +0800

    login

--- huigouWeb/resources/src/router/index.js
+++ huigouWeb/resources/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '@/components/user/login'
+import Register from '@/components/user/register'
+import Forget from '@/components/user/forget'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/login.html',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register.html',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/forget.html',
+      name: 'Forget',
+      component: Forget
     }
   ]
 })
